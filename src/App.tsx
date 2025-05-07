b9eb32e36c32feb8559f389aa57d0721c8399484
Shaik mahammad jani <68095466+skjani314@users.noreply.github.com>
2025-05-07 16:12:35 +0000

    90%

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/layout";
-import HomePage from "./pages/HomePage";
-import InboxPage from "./pages/InboxPage";
-import CalendarPage from "./pages/CalenderPage";
-import SearchPage from "./pages/SearchPage";
-import SettingsPage from "./pages/SettingsPage";
+// import Layout from "./components/Layout/layout";
+// import HomePage from "./pages/HomePage";
+// import InboxPage from "./pages/InboxPage";
+// import CalendarPage from "./pages/CalenderPage";
+// import SearchPage from "./pages/SearchPage";
+// import SettingsPage from "./pages/SettingsPage";
+import Page from "./components/Layout/dashboard";
+import './App.css'
 
 function App() {
   return (
     <Router>
-      <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/inbox" element={<InboxPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/" element={<Page/>} />
         </Routes>
-      </Layout>
     </Router>
   );
 }
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,8 +1,9 @@
+"use client"
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { type VariantProps } from "class-variance-authority";
+import { type VariantProps, cva } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
-import { cva } from "class-variance-authority";
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
